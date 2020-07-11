5baa9f970ef362d4039ad4d764310c98ed893126
LAPTOP-A3H578KA\Neal Patrick Padama <nppadama@gmail.com>
2020-07-11 22:38:17 +0800

    Added first exercise - Solar system explorer - Solar system planets - Ship - Movement - Texture - Rotate for planets

---
+++ Solar System Explorer/Assets/Script/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // going right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+
+        // going left
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(1, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+
+        // going upwards
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1) * Time.deltaTime * movementSpeed;
+        }
+
+        // going backwards
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, 0, 1) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
