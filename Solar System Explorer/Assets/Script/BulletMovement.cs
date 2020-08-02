45e37283b39394d5e17419921f15b617ea49ddc2
LAPTOP-A3H578KA\Neal Patrick Padama <nppadama@gmail.com>
2020-08-02 13:09:14 +0800

    Added module 2 task (4 missiles, nozzles, destroy)

---
+++ Solar System Explorer/Assets/Script/BulletMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [SerializeField]public float speed;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 5);
+    }
+}
---
+++ Solar System Explorer/Assets/Script/BulletShooter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour
+{
+    public List<Transform> nozzles;
+
+    public GameObject bulletPrefab;
+    public GameObject fastBulletPrefab;
+    public GameObject bombPrefab;
+    public GameObject missilePrefab; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            for(int i = 0; i < nozzles.Count; i++)
+            {
+                Instantiate(bulletPrefab, nozzles[i].transform.position, transform.rotation);
+            }
+            /*
+            Instantiate(bulletPrefab, nozzle1.transform.position, transform.rotation);
+            Instantiate(bulletPrefab, nozzle2.transform.position, transform.rotation);
+            Instantiate(bulletPrefab, nozzle3.transform.position, transform.rotation);
+            Instantiate(bulletPrefab, nozzle4.transform.position, transform.rotation);*/
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            for (int i = 0; i < nozzles.Count; i++)
+            {
+                Instantiate(fastBulletPrefab, nozzles[i].transform.position, transform.rotation);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < nozzles.Count; i++)
+            {
+                Instantiate(bombPrefab, nozzles[i].transform.position, transform.rotation);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            for (int i = 0; i < nozzles.Count; i++)
+            {
+                Instantiate(missilePrefab, nozzles[i].transform.position, transform.rotation);
+            }
+        }
+    }
+}
